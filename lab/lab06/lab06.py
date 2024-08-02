ec85fd6ffd571fe4201c6d0e70703f46a9012469
aric <3362484152@qq.com>
2024-08-02 20:34:23 +0800

    Finish lab06

---
+++ lab/lab06/lab06.py
@@ -0,0 +1,160 @@
+def count_occurrences(t, n, x):
+    """Return the number of times that x is equal to one of the
+    first n elements of iterator t.
+
+    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(s, 10, 9)
+    3
+    >>> t = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(t, 3, 10)
+    2
+    >>> u = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+    >>> count_occurrences(u, 1, 3)  # Only iterate over 3
+    1
+    >>> count_occurrences(u, 3, 2)  # Only iterate over 2, 2, 2
+    3
+    >>> list(u)                     # Ensure that the iterator has advanced the right amount
+    [1, 2, 1, 4, 4, 5, 5, 5]
+    >>> v = iter([4, 1, 6, 6, 7, 7, 6, 6, 2, 2, 2, 5])
+    >>> count_occurrences(v, 6, 6)
+    2
+    """
+    ans = 0
+    for i in range(n):
+        if next(t) == x:
+            ans = ans + 1
+    return ans
+
+
+def hailstone(num):
+    """Yields the elements of the hailstone sequence starting at n.
+
+    >>> for num in hailstone(10):
+    ...     print(num)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    ans = []
+    while num is not 1:
+        ans.extend([num])
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+    ans.append(1)
+    return ans
+
+
+def merge(incr_a, incr_b):
+    """Yield the elements of strictly increasing iterables incr_a and incr_b, removing
+    repeats. Assume that incr_a and incr_b have no repeats. incr_a or incr_b may or may not
+    be infinite sequences.
+
+    >>> m = merge([0, 2, 4, 6, 8, 10, 12, 14], [0, 3, 6, 9, 12, 15])
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
+    >>> def big(n):
+    ...    k = 0
+    ...    while True: yield k; k += n
+    >>> m = merge(big(2), big(3))
+    >>> [next(m) for _ in range(11)]
+    [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
+    """
+    iter_a, iter_b = iter(incr_a), iter(incr_b)
+    next_a, next_b = next(iter_a, None), next(iter_b, None)
+    while next_a is not None and next_b is not None:
+        if next_a < next_b:
+            yield next_a
+            next_a = next(iter_a, None)
+        elif next_a > next_b:
+            yield next_b
+            next_b = next(iter_b, None)
+        else:
+            yield next_a
+            next_a = next(iter_a, None)
+            next_b = next(iter_b, None)
+
+    while next_a is not None:
+        yield next_a
+        next_a = next(iter_a, None)
+    while next_b is not None:
+        yield next_b
+        next_b = next(iter_b, None)
+
+
+def deep_map(f, s):
+    """Replace all non-list elements x with f(x) in the nested list s.
+
+    >>> six = [1, 2, [3, [4], 5], 6]
+    >>> deep_map(lambda x: x * x, six)
+    >>> six
+    [1, 4, [9, [16], 25], 36]
+    >>> # Check that you're not making new lists
+    >>> s = [3, [1, [4, [1]]]]
+    >>> s1 = s[1]
+    >>> s2 = s1[1]
+    >>> s3 = s2[1]
+    >>> deep_map(lambda x: x + 1, s)
+    >>> s
+    [4, [2, [5, [2]]]]
+    >>> s1 is s[1]
+    True
+    >>> s2 is s1[1]
+    True
+    >>> s3 is s2[1]
+    True
+    """
+    for i in range(len(s)):
+        if type(s[i]) == list:
+            deep_map(f, s[i])
+        else:
+            s[i] = f(s[i])
+
+
+def buy(required_fruits, prices, total_amount):
+    """Print ways to buy some of each fruit so that the sum of prices is amount.
+
+    >>> prices = {'oranges': 4, 'apples': 3, 'bananas': 2, 'kiwis': 9}
+    >>> buy(['apples', 'oranges', 'bananas'], prices, 12)
+    [2 apples][1 orange][1 banana]
+    >>> buy(['apples', 'oranges', 'bananas'], prices, 16)
+    [2 apples][1 orange][3 bananas]
+    [2 apples][2 oranges][1 banana]
+    >>> buy(['apples', 'kiwis'], prices, 36)
+    [3 apples][3 kiwis]
+    [6 apples][2 kiwis]
+    [9 apples][1 kiwi]
+    """
+    def add(fruits, amount, cart):
+        if fruits == [] and amount == 0:
+            print(cart)
+        elif fruits and amount > 0:
+            fruit = fruits[0]
+            price = prices[fruit]
+            for k in range(1, amount // price + 1):
+                add(fruits[1:], amount - k * price, cart + display(fruit, k))
+    add(required_fruits, total_amount, '')
+
+
+def display(fruit, count):
+    """Display a count of a fruit in square brackets.
+
+    >>> display('apples', 3)
+    '[3 apples]'
+    >>> display('apples', 1)
+    '[1 apple]'
+    """
+    assert count >= 1 and fruit[-1] == 's'
+    if count == 1:
+        fruit = fruit[:-1]  # get rid of the plural s
+    return '[' + str(count) + ' ' + fruit + ']'
+
+
+
